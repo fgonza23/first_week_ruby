65a158ad64a113b9006e3e8c383a7dc79b72fb11
fgonza23 <fgonza0267@gmail.com>
2018-08-20 23:33:47 -0500

    added to code

--- untitled.rb
+++ untitled.rb
@@ -1,6 +1,6 @@
-fruit_one = ["fruit" => "apple", "price" => "2.50", "color" => "red", "size" => "small" ]
-fruit_two = [fruit: "pear", price: 3, color: "orange", size: "small" ]
-fruit_three = ["fruit" => "orange", "price" => "1.50", "color" => "orange", "size" => "small"]
+# fruit_one = ["fruit" => "apple", "price" => 2, "color" => "red", "size" => "small" ]
+# fruit_two = [fruit: "pear", price: 3, color: "green", size: "small" ]
+# fruit_three = ["fruit" => "orange", "price" => 1, "color" => "orange", "size" => "small"]
 
 
 Class Fruit 
@@ -17,6 +17,28 @@ Class Fruit
     puts "#{ @fruit }, #{ @price }, #{ @color }, #{ @size } "
   end
 
+  def fruit
+    @fruit
+  end
 
+  def price
+    @price
+  end
+
+  def color
+    @color
+  end
+
+  def size
+    @size
+  end
+
+  def price=(new_value)
+    @price = new_value
+  end
+
+fruit_one = Fruit.new("Apple", 2, "red", "small")
+fruit_two = Fruit.new("Watermellon", 3, "green", "big")
+fruit_three = Fruit.new("Orange", 2, "orange", "small")
 
 end
